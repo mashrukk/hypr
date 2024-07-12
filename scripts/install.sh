084a27ff025285f6293a6631ebcf975e71a8b095
Mashruk Kabir <120426030+mashrukk@users.noreply.github.com>
2024-07-12 22:05:57 +0000

    Add files via upload

---
+++ scripts/install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo "Installing AUR helper..."
+git clone https://aur.archlinux.org/paru-bin.git
+cd paru-bin || exit
+makepkg -si --noconfirm
+cd ..
+rm -rf paru-bin
+
+echo "Installing packages"
+paru -S hyprland waybar wofi swww noto-fonts noto-fonts-emoji alacritty mpv pavucontrol playerctl otf-font-awesome polkit-gnome gnome-themes-extra papirus-icon-theme blueman
+sudo systemctl enable bluetooth
+
+# Thunar
+./thunar.sh
+clear
+cd 
+
+# Apps
+paru -S --noconfirm obsidian signal-desktop syncthing keepassxc
+paru -S --noconfirm librewolf-bin
+clear
+paru -S --noconfirm qview-git
+clear
+paru -S --noconfirm vscodium-bin
+clear
+paru -S --noconfirm nwg-look-bin
+
+echo "Configuring Hyprland Desktop Environment..."
+mkdir -p Pictures/wallpapers
+mkdir -p Documents/Shared
+mkdir Videos
+cd hypr/
+mv alacritty/ ~/.config/
+mv wofi/ ~/.config/
+mv wall.jpg ~/Pictures/wallpapers/
+cd
+mv hypr/hyprland.conf ~/.config/hypr/
+sudo mv hypr/waybar/config.jsonc /etc/xdg/waybar/
+sudo mv hypr/waybar/style.css /etc/xdg/waybar/
+sudo rm /usr/share/hyprland/*
+clear
+Hyprland
