73fb177cb75cf29033a8142e79cfd6385faddcea
Mashruk Kabir <120426030+mashrukk@users.noreply.github.com>
2024-07-13 09:22:51 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -36,9 +36,9 @@ mv wofi/ ~/.config/
 mv wall.jpg ~/Pictures/wallpapers/
 cd
 sudo rm /usr/share/hyprland/*
-mv hypr/hyprland.conf ~/.config/hypr/
-mv hypr/hyprlock.conf ~/.config/hypr/
-sudo mv hypr/waybar/config.jsonc /etc/xdg/waybar/
-sudo mv hypr/waybar/style.css /etc/xdg/waybar/
+mv ~/hypr/hyprland.conf ~/.config/hypr/
+mv ~/hypr/hyprlock.conf ~/.config/hypr/
+sudo mv ~/hypr/waybar/config.jsonc /etc/xdg/waybar/
+sudo mv ~/hypr/waybar/style.css /etc/xdg/waybar/
 clear
 Hyprland
