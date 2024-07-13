8f17f5cc4b368b6fca51df5480d554a611503dce
Mashruk Kabir <120426030+mashrukk@users.noreply.github.com>
2024-07-13 09:23:38 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -40,5 +40,6 @@ mv ~/hypr/hyprland.conf ~/.config/hypr/
 mv ~/hypr/hyprlock.conf ~/.config/hypr/
 sudo mv ~/hypr/waybar/config.jsonc /etc/xdg/waybar/
 sudo mv ~/hypr/waybar/style.css /etc/xdg/waybar/
+cd
 clear
 Hyprland
