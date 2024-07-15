503ed9dbbe3e549661e42aea59b4954288e4fc1f
Mashruk Kabir <120426030+mashrukk@users.noreply.github.com>
2024-07-15 05:22:04 +0000

    Update post.sh

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 cd
+mv ~/hypr/alacritty ~/.config/
+mv ~/hypr/wofi ~/.config/
+mkdir -p ~/Pictures/wallpapers/
+mkdir -p ~/Documents/Shared/
+mv ~/hypr/wall.jpg ~/Pictures/wallpapers/
 sudo rm /usr/share/hyprland/*
-
 mv ~/hypr/hyprland.conf ~/.config/hypr/
-
 mv ~/hypr/hyprlock.conf ~/.config/hypr/
-
 sudo mv ~/hypr/waybar/config.jsonc /etc/xdg/waybar/
-
 sudo mv ~/hypr/waybar/style.css /etc/xdg/waybar/
-
-cd
-
 clear
-
 Hyprland
