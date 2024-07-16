9f3ed9b7a4886dfc14785441b7e9c12bf7f073cc
Mashruk Kabir <120426030+mashrukk@users.noreply.github.com>
2024-07-16 19:14:43 +0000

    Update post.sh

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-mv ~/hypr/alacritty ~/.config/
-mv ~/hypr/wofi ~/.config/
 mkdir -p ~/Pictures/wallpapers/
 mkdir -p ~/Pictures/Screenshots/
 mkdir -p ~/Documents/Shared/
+mkdir -p ~/.local/share/icons/
+mv ~/hypr/cursor ~/.local/share/icons/
+mv ~/hypr/alacritty ~/.config/
+mv ~/hypr/wofi ~/.config/
 mv ~/hypr/wall.jpg ~/Pictures/wallpapers/
 mv ~/hypr/hyprland.conf ~/.config/hypr/
 mv ~/hypr/hyprlock.conf ~/.config/hypr/
@@ -13,11 +15,6 @@ sudo mv ~/hypr/waybar/style.css /etc/xdg/waybar/
 cd ~/hypr/scripts
 rm install.sh thunar.sh post.sh
 xdg-settings set default-web-browser librewolf.desktop
-mkdir -p ~/.local/share/icons/
-curl -L -o ~/.local/share/icons/Bibata_Cursor.tar.gz https://github.com/ful1e5/Bibata_Cursor/archive/v2.0.7.tar.gz
-cd ~/.local/share/icons
-tar -xvf Bibata_Cursor.tar.gz
-rm Bibata_Cursor.tar.gz
 cd
 clear
 Hyprland
