c65072558bfdf0b6dde389f0529fb9596ae7b40e
Mashruk Kabir <120426030+mashrukk@users.noreply.github.com>
2024-07-16 15:13:51 +0000

    Update th.sh

--- th.sh
+++ th.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-# Update repositories and install necessary packages
-sudo pacman -Syu --noconfirm thunar gvfs gvfs-mtp gvfs-gphoto2 ntfs-3g
+# Install gvfs for automounting support
+sudo pacman -S --noconfirm gvfs
 
-# Configure Thunar to automatically mount drives and devices
-cat <<EOF | sudo tee /etc/polkit-1/rules.d/49-thunar-mount.rules
-polkit.addRule(function(action, subject) {
-    if (action.id == "org.freedesktop.udisks2.filesystem-mount-system" && subject.isInGroup("storage")) {
-        return polkit.Result.YES;
-    }
-});
-EOF
-
-# Add user to 'storage' group to allow mounting
-sudo groupadd -f storage
-sudo gpasswd -a "$(whoami)" storage
-
-# Enable and start GVFS services for automounting
-systemctl --user daemon-reload
-systemctl --user start gvfs-udisks2-volume-monitor.service
-systemctl --user start gvfs-gphoto2-volume-monitor.service
-systemctl --user start gvfs-mtp-volume-monitor.service
+# Enable and start GVFS services for user session
+systemctl --user enable --now gvfs-udisks2-volume-monitor.service
+systemctl --user enable --now gvfs-goa-volume-monitor.service
