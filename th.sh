937c4f9fc1e7a3241d82c9705ad26490456a171b
Mashruk Kabir <120426030+mashrukk@users.noreply.github.com>
2024-07-16 17:01:48 +0000

    Delete th.sh

--- th.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install gvfs for automounting support
-sudo pacman -S --noconfirm gvfs
-
-# Enable and start GVFS services for user session
-systemctl --user enable --now gvfs-udisks2-volume-monitor.service
-systemctl --user enable --now gvfs-goa-volume-monitor.service
