94b9b0bbcde52ed1dc1b52c8380cd56161bbf2f4
Mashruk Kabir <120426030+mashrukk@users.noreply.github.com>
2024-07-16 19:01:03 +0000

    Update post.sh

--- scripts/post.sh
+++ scripts/post.sh
@@ -13,6 +13,11 @@ sudo mv ~/hypr/waybar/style.css /etc/xdg/waybar/
 cd ~/hypr/scripts
 rm install.sh thunar.sh post.sh
 xdg-settings set default-web-browser librewolf.desktop
-clear
+mkdir -p ~/.local/share/icons/
+curl -L -o ~/.local/share/icons/Bibata_Cursor.tar.gz https://github.com/ful1e5/Bibata_Cursor/archive/v2.0.7.tar.gz
+cd ~/.local/share/icons
+tar -xvf Bibata_Cursor.tar.gz
+rm Bibata_Cursor.tar.gz
 cd
+clear
 Hyprland
