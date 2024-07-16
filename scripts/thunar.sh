52cd43699ddcea1031641dfaca02ed86b6c51a37
Mashruk Kabir <120426030+mashrukk@users.noreply.github.com>
2024-07-16 14:48:18 +0000

    Update thunar.sh

--- scripts/thunar.sh
+++ scripts/thunar.sh
@@ -1,52 +1,22 @@
 #!/bin/bash
 
-thunar_packages=(
-    thunar
-    thunar-volman
+# Thunar and related packages
+thunar=(
+    thunar 
+    thunar-volman 
     tumbler
     ffmpegthumbnailer
-    udisks2
 )
 
-# Check if a package is already installed
-check_package_installed() {
-    pacman -Q "$1" &>/dev/null
-}
-
-# Install packages if not already installed
-for package in "${thunar_packages[@]}"; do
-    if ! check_package_installed "$package"; then
-        sudo pacman -S --noconfirm "$package"
-        if [ $? -ne 0 ]; then
-            echo "Error: Failed to install $package."
-            exit 1
-        fi
-    else
-        echo "$package is already installed."
-    fi
+# Install Thunar and related packages
+for THUNAR in "${thunar[@]}"; do
+    sudo pacman -S --noconfirm "$THUNAR"
 done
 
-# Check if udisks2 service is running
-if ! systemctl is-active --quiet udisks2; then
-    echo "udisks2 service is not running. Starting udisks2..."
-    sudo systemctl start udisks2
-fi
-
-# Thunar volume management rules setup function
-setup_thunar_volman_rules() {
-    local volman_config_path=~/.config/Thunar/uca.xml
-    local assets_dir="$(dirname "$0")/assets"  # Assuming assets folder is in the same directory as this script
-
-    if [ ! -f "$volman_config_path" ]; then
-        mkdir -p ~/.config/Thunar  # Ensure Thunar config directory exists
-        cp "$assets_dir/uca.xml" "$volman_config_path"
-        sed -i 's|<mount>udisks2://|<mount>|g' "$volman_config_path"  # Use udisks2 for mounting
-    else
-        echo "Thunar Volume Management rules already exist, no need to set up."
+# Check and copy configuration files if they don't exist
+for DIR1 in gtk-3.0 Thunar xfce4; do
+    DIRPATH=~/.config/$DIR1
+    if [ ! -d "$DIRPATH" ]; then
+        cp -r assets/$DIR1 ~/.config/ && echo "Copied $DIR1 config files."
     fi
-}
-
-setup_thunar_volman_rules
-
-echo "Thunar and essential related packages installed and configured successfully."
-
+done
