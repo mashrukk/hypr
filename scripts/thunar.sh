35f02afc40b479e1e812d89dd11f8a19897ae8c2
Mashruk Kabir <120426030+mashrukk@users.noreply.github.com>
2024-07-17 05:40:35 +0000

    Update thunar.sh

--- scripts/thunar.sh
+++ scripts/thunar.sh
@@ -10,30 +10,37 @@ thunar_packages=(
     ntfs-3g
 )
 
-for package in "${thunar_packages[@]}"; do
-    sudo pacman -S --noconfirm "$package"
-    if [ $? -ne 0 ]; then
-        echo "Error: Failed to install $package."
-        exit 1
+install_packages() {
+    for package in "${thunar_packages[@]}"; do
+        sudo pacman -S --noconfirm "$package"
+        if [ $? -ne 0 ]; then
+            echo "Error: Failed to install $package."
+            exit 1
+        fi
+    done
+}
+
+check_gvfs_status() {
+    if ! pgrep -x "gvfsd" >/dev/null; then
+        echo "Warning: gvfs is not running. Please ensure it is started for proper Thunar functionality."
     fi
-done
+}
 
-# Check if gvfs is running
-if ! pgrep -x "gvfsd" >/dev/null; then
-    echo "gvfs is not running. Please ensure it is started for proper Thunar functionality."
-fi
-
-# Thunar volume management rules
 setup_thunar_volman_rules() {
     local volman_config_path=~/.config/Thunar/uca.xml
     if [ ! -f "$volman_config_path" ]; then
         echo "Setting up Thunar Volume Management rules..."
-        cp assets/uca.xml ~/.config/Thunar/
+        cp assets/uca.xml "$volman_config_path"
         echo "Thunar Volume Management rules set up successfully."
     else
         echo "Thunar Volume Management rules already exist, no need to set up."
     fi
 }
+
+echo "Installing Thunar and related packages..."
+install_packages
+
+check_gvfs_status
 
 setup_thunar_volman_rules
 
@@ -44,24 +51,20 @@ lsblk -o NAME,SIZE,TYPE,MOUNTPOINT,UUID
 
 read -p "Enter the drive name (e.g., sdb): " drive_name
 
-# Get UUID of the selected drive
 uuid=$(lsblk -no UUID /dev/"$drive_name")
 
 if [ -z "$uuid" ]; then
-  echo "UUID not found for /dev/$drive_name"
-  exit 1
+    echo "Error: UUID not found for /dev/$drive_name"
+    exit 1
 fi
 
-# Set mount point and filesystem type
 mount_point="/media/drive"
 filesystem_type="auto"
 options="defaults"
 
-# Add entry to /etc/fstab
 echo "UUID=$uuid $mount_point $filesystem_type $options 0 0" | sudo tee -a /etc/fstab
 
 tail -n 1 /etc/fstab
 
 sudo mount -a
-
 echo "Thunar and essential related packages installed and configured successfully."
