ae03ef594dc9b3e46b30d139b183d99c162b765f
Mashruk Kabir <120426030+mashrukk@users.noreply.github.com>
2024-08-02 17:17:57 +0600

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,7 +14,7 @@ cd ..
 rm -rf paru-bin
 
 echo "Installing packages"
-paru -S --noconfirm hyprland waybar wofi swww hyprlock cliphist xdg-desktop-portal-hyprland noto-fonts noto-fonts-emoji alacritty mpv pulsemixer grim slurp playerctl polkit-gnome gnome-themes-extra papirus-icon-theme blueman fastfetch btop
+paru -S --noconfirm hyprland waybar wofi swww hyprlock cliphist xdg-desktop-portal-hyprland noto-fonts noto-fonts-emoji alacritty mpv pulsemixer grim slurp playerctl polkit-gnome gnome-themes-extra papirus-icon-theme nwg-look blueman fastfetch btop
 sudo systemctl enable bluetooth
 
 # Thunar
@@ -29,7 +29,6 @@ paru -S --noconfirm qview-git
 clear
 paru -S --noconfirm vscodium-bin
 clear
-paru -S --noconfirm nwg-look
 cd
 sudo rm /usr/share/hyprland/*
 Hyprland
